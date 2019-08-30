c45ec398b17c547c510117306120d83d7fce1543
Henry Liu <365159110@qq.com>
2019-08-30 09:49:15 +0800

    common/utils: copy file, load file

---
+++ common/hl-utils.h
@@ -0,0 +1,62 @@
+
+/**
+ * hl-utils.h
+ * utils
+ * Henry 2019-05-14
+ * */
+
+#ifndef __HL_UTILES_H__
+#define __HL_UTILES_H__
+
+#include <string.h>
+
+#include <easylogging++.h>
+#include <LogHelper.h>
+
+#define DEBUG_OUTPUT  0
+
+#define HL_CHECK_RESULT_RETURN_VOID(r,e,s) \
+    if (r == e) {              \
+        printf("%s\n", s);     \
+        return ;               \
+    }
+
+#define HL_CHECK_RESULT_RETURN(r,e,s,t) \
+    if (r == e) {              \
+        printf("%s\n", s);     \
+        return t;              \
+    }
+
+#define HL_CHECK_RESULT_EXIT(r,e,s) \
+    if (r == e) {              \
+        printf("%s\n", s);     \
+        exit(-1);              \
+    }
+
+#define HL_XFREE(b) \
+    if (b) free(b); \
+    b = NULL;
+
+#define Hl_FILE_MAX_SIZE       10240
+#define HL_LOG_BUFFER_SIZE     1024
+
+/**
+ * copy file
+ * src: filename of source file
+ * dest: filename of destiantion file
+ *
+ * return 0 for success, -1 for failed
+ * */
+extern int cp_file(char *src, char* dest);
+
+/**
+ * load file
+ * filename: file to be loaded
+ * buf: store result into buf
+ * size: size of buf
+ *
+ * return file size for success, -1 for failed
+ * */
+extern size_t load_file(const char *filename, char *buf, const size_t size);
+
+#endif
